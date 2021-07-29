f16d3e0e188411d47bb44bda115c09b9c948824c
guilhermetwelve0 <guilherme.cabral999@hotmail.com>
2021-07-29 16:13:34 -0300

    primeiro commit

---
+++ database/database.js
@@ -0,0 +1,8 @@
+const Sequilize = require('sequelize');
+
+const connection = new Sequilize('guiaperguntas','root','123456',{
+    host: 'localhost',
+    dialect: 'mysql',
+});
+
+module.exports = connection;
